8ccf508693cbc029446eb21bc9961a46ea8e54cc
Aynes <ceh4o@mail.ru>
2023-10-31 00:15:00 +0300

    product page tests added

---
+++ test_product_page.py
@@ -0,0 +1,13 @@
+from .pages.product_page import ProductPage
+import pytest
+import selenium
+import time
+import math
+from selenium.webdriver.common.by import By
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.solve_quiz_and_get_code()
